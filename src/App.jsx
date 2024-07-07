12585da0eb6f9e61d2d458d6b2015714168f130c
Danielroxs <danroxsll@gmail.com>
2024-07-07 13:15:29 -0600

    Model 3D Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+import Features from "./components/Features";
+
+import * as Sentry from "@sentry/react";
 
 const App = () => {
   return (
@@ -8,8 +12,10 @@ const App = () => {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
+      <Features />
     </main>
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
